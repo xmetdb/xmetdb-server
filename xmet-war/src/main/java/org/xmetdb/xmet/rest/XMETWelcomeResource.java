dcf61887e673061df20b144c8e0622a1c7dd121b
ngn <jeliazkova.nina@gmail.com>
2012-09-24 21:06:39 +0300

    pass the original request

--- xmet-war/src/main/java/org/xmetdb/xmet/rest/XMETWelcomeResource.java
+++ xmet-war/src/main/java/org/xmetdb/xmet/rest/XMETWelcomeResource.java
@@ -30,6 +30,7 @@ public class XMETWelcomeResource extends ServerResource {
 	        	map.put("username", getClientInfo().getUser().getIdentifier());
 	        map.put("creator","IdeaConsult Ltd.");
 	        map.put("xmet_root",getRequest().getRootRef().toString());
+	        map.put("xmet_request",getRequest().getResourceRef().toString());
 	        map.put(Resources.Config.xmet_email.name(),((TaskApplication)getApplication()).getProperty(Resources.Config.xmet_email.name()));
 	        map.put(Resources.Config.xmet_about.name(),((TaskApplication)getApplication()).getProperty(Resources.Config.xmet_about.name()));
 	        map.put(Resources.Config.xmet_guide.name(),((TaskApplication)getApplication()).getProperty(Resources.Config.xmet_guide.name()));
